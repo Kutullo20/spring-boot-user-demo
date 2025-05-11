3a368a56a348537f15557a023dae0cc9504cd863
Kutullo Letageng <u21585513@tuks.co.za>
2025-05-11 18:41:46 +0200

    test: add JUnit 5 imports to UserServiceTests for upcoming tests

--- app/src/test/java/org/springbootuserdemo/service/UserServiceTests.java
+++ app/src/test/java/org/springbootuserdemo/service/UserServiceTests.java
@@ -1,5 +1,9 @@
 package org.springbootuserdemo.service;
 
+// JUnit 5 imports
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UserServiceTests {
     
 }
