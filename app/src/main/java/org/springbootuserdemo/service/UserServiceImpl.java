fef8ccffb6e93cff11209cc00c8806b21d5a15e3
Kutullo Letageng <u21585513@tuks.co.za>
2025-05-13 21:24:58 +0200

    feat(service): implement UserServiceImpl with addUser method and FakeRepo injection

--- app/src/main/java/org/springbootuserdemo/service/UserServiceImpl.java
+++ app/src/main/java/org/springbootuserdemo/service/UserServiceImpl.java
@@ -1,5 +1,26 @@
 package org.springbootuserdemo.service;
 
-public class UserServiceImpl {
-    
+import org.springbootuserdemo.model.User;
+import org.springbootuserdemo.repository.FakeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+// Marks this class as a Spring service component
+@Service  
+public class UserServiceImpl implements UserService {
+    private final FakeRepo fakeRepo;
+
+    //injects FakeRepo dependency
+    @Autowired  
+    public UserServiceImpl(FakeRepo fakeRepo) {
+        this.fakeRepo = fakeRepo;
+    }
+
+    @Override
+    public void addUser(String name, String surname) {
+        // Create new user and add to repository
+        User newUser = new User(name, surname);
+        fakeRepo.insertUser(newUser);
+    }
+   
 }
