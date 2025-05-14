4aed08f90da6fac8b82de3d91c5665bb2a6a262c
Kutullo Letageng <u21585513@tuks.co.za>
2025-05-14 23:20:21 +0200

    feat(service): add getUser method to UserService with ID lookup

--- app/src/main/java/org/springbootuserdemo/service/UserService.java
+++ app/src/main/java/org/springbootuserdemo/service/UserService.java
@@ -1,6 +1,11 @@
 package org.springbootuserdemo.service;
 
+import org.springbootuserdemo.model.User;
+
 public interface UserService {
     // Method to add a new user
     void addUser(String name, String surname); 
+    
+    //Returns the User with the given ID or null if not found
+    User getUser(String id); 
 }
