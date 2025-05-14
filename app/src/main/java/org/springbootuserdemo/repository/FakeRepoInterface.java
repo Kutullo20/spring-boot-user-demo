495cb365b5f7b5ff0b12be606cae30524021c26d
Kutullo Letageng <u21585513@tuks.co.za>
2025-05-14 23:09:55 +0200

    feat(repository): add findUserById method to FakeRepoInterface

--- app/src/main/java/org/springbootuserdemo/repository/FakeRepoInterface.java
+++ app/src/main/java/org/springbootuserdemo/repository/FakeRepoInterface.java
@@ -5,5 +5,8 @@ import org.springbootuserdemo.model.User;
 public interface FakeRepoInterface {
     // Method to add a new user
      void insertUser(User user); 
+
+     // Method to find/get user
+     User findUserById(String id); 
     
 }
