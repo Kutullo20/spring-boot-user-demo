8e6939b193c8992476739b5e86466d551a577184
Kutullo Letageng <u21585513@tuks.co.za>
2025-05-15 21:29:46 +0200

    chore(repository): remove unused Collectors import in FakeRepo

--- app/src/main/java/org/springbootuserdemo/repository/FakeRepo.java
+++ app/src/main/java/org/springbootuserdemo/repository/FakeRepo.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Repository
 public class FakeRepo implements FakeRepoInterface {
